d3f34a7482ffde58ad434546322356fe31b2c8b4
peteykienzzz <pmatczak2@gmail.com>
2022-06-13 17:34:24 -0700

    tested a move_decimal function, and calculating_sO2. moved it in the correct order. along in the main() code

--- wine.py
+++ wine.py
@@ -7,12 +7,6 @@ def main():
     pass
 
 
-def mixed_in_s02(value):
-    try:
-        value == int(value)
-    except ValueError:
-        [put_buttons(['A'], onclick=put_text)]
-
 def moving_decimal(num):
     num /= 1000.
     return num
@@ -24,7 +18,7 @@ def calculating_sO2(desired_sO2, gallons):
 def temperature_convertor(temperature):
     celsius = int(((temperature - 32) * 5) / 9)
     return celsius
-
+# I be think he was explaining the ton to crushed must gallons but we want to input actually measurements gallons of must or juice. But yes one ton is 2000 lbs.
 
 def main():
     '''
@@ -47,14 +41,14 @@ def main():
                                required=True)
     put_text('The date Grapes were picked on:  %s' % date_grapes_picked)
 
-    tons = input("How many lbs were picked?", type=NUMBER, placeholder="Weight of product", required=True)
+    tons = input("How many lbs were picked?", type=FLOAT, placeholder="Weight of product", required=True)
     put_text("Total weight: %slbs" % tons)
 
     cost = input('Cost', type=NUMBER, placeholder='What was the cost of purchase?', required=True)
     put_text("Cost was: $%s" % cost)
 
     date_crushed = input("Date grapes crushed:", placeholder="Date grapes crushed", required=True)
-    gallons = input("Gallons produced after crushing", type=NUMBER, placeholder="Gallons produced", required=True)
+    gallons = input("Gallons produced after crushing", type=FLOAT, placeholder="Gallons produced", required=True)
 
     with put_collapse("Date crushed | Weight | Crushed Gallons"):
         put_table([
@@ -62,11 +56,11 @@ def main():
             [put_text("%s" % date_crushed), put_text("%slbs" % tons), put_text("%s" % gallons)]
         ])
 
-    s02 = input(placeholder='Added sO2')
+    s02 = input(type=NUMBER, placeholder='bring up sO2')
     with put_collapse('Added sO2'):
         put_table([
-            ['sO2 in grams'],
-            [put_text(s02)],
+            ['Add sO2 grams'],
+            [put_text(calculating_sO2(moving_decimal(s02), gallons), "g")]
         ])
 
     temperature = input("Temperature in Farenhite", type=NUMBER, placeholder="Temperature of wine", required=True)
@@ -76,6 +70,12 @@ def main():
             ["F", "C"],
             [put_text(temperature), put_text(temperature_convertor(temperature))]
         ])
+
+    data = input_group("Brix info", [
+        input('Input your name', name='name'),
+        input('Input your age', name='age')
+    ])
+    put_text(data['name'], data['age'])
 
 if __name__ == "__main__":
     main()
