ddeb3e7c38b9895936c9c468dde8d1390a840a10
peteykienzzz <pmatczak2@gmail.com>
2022-06-16 19:03:21 -0700

    refactored code with updated functions

--- wine.py
+++ wine.py
@@ -6,18 +6,33 @@ from pywebio.input import *
 def main():
     pass
 
+def date_brix():
+    occasion = input("Enter current date", placeholder="Enter today's date", required=True)
+    sugar = input("Enter brix")
+    return occasion, sugar
+
+def sO2():
+    sugar = float(input(type=NUMBER, placeholder='bring up sO2'))
+    return sugar
 
 def moving_decimal(num):
     num /= 1000.
     return num
 
+
 def calculating_sO2(desired_sO2, gallons):
     bring_up = desired_sO2 * 3.785 * gallons / 0.57
     return round(bring_up, 2)
 
-def temperature_convertor(temperature):
-    celsius = int(((temperature - 32) * 5) / 9)
-    return celsius
+
+def temperature_convertor():
+    temp = float(input("Enter temp: "))
+    celsius = round((temp - 32) * 5 / 9, 1)
+    return temp, celsius
+
+
+
+
 # I be think he was explaining the ton to crushed must gallons but we want to input actually measurements gallons of must or juice. But yes one ton is 2000 lbs.
 
 def main():
@@ -28,7 +43,7 @@ def main():
     put_markdown("# **Leony's Inventory Manegmant System**")
 
     year_of_grape = input("The year grape picked", placeholder="Year picked",
-                          required=True)
+                           required=True)
     put_text("The year: %s" % year_of_grape)
 
     ava = input("Name of AVA:", placeholder="Whats the AVA name?", required=True)
@@ -38,7 +53,7 @@ def main():
     put_text("Name of vine: %s" % name_of_vine)
 
     date_grapes_picked = input('Date picked:', placeholder='The exact date grapes were picked',
-                               required=True)
+                                required=True)
     put_text('The date Grapes were picked on:  %s' % date_grapes_picked)
 
     tons = input("How many lbs were picked?", type=FLOAT, placeholder="Weight of product", required=True)
@@ -56,26 +71,29 @@ def main():
             [put_text("%s" % date_crushed), put_text("%slbs" % tons), put_text("%s" % gallons)]
         ])
 
-    s02 = input(type=NUMBER, placeholder='bring up sO2')
+    sweet = sO2()
     with put_collapse('Added sO2'):
         put_table([
             ['Add sO2 grams'],
-            [put_text(calculating_sO2(moving_decimal(s02), gallons), "g")]
+            [put_text(calculating_sO2(moving_decimal(sweet), gallons), "g")]
         ])
 
-    temperature = input("Temperature in Farenhite", type=NUMBER, placeholder="Temperature of wine", required=True)
-
+    fahrenheit, celsius = temperature_convertor()
     with put_collapse("Temperature"):
         put_table([
-            ["F", "C"],
-            [put_text(temperature), put_text(temperature_convertor(temperature))]
+            ["Fahrenheit", "Celsius"],
+            [put_text(fahrenheit), put_text(celsius)]
         ])
 
-    data = input_group("Brix info", [
-        input('Input your name', name='name'),
-        input('Input your age', name='age')
-    ])
-    put_text(data['name'], data['age'])
+    date, brix = date_brix()
+    fahrenheit, celsius = temperature_convertor()
+    with put_collapse("Today's date | Brix | Temperature"):
+        put_table([
+            ["Today's date", "Brix", "Fahrenheit", "Celsius"],
+            [put_text(date), put_text(brix), put_text(fahrenheit), put_text(celsius)]
+        ])
+
 
 if __name__ == "__main__":
     main()
+
